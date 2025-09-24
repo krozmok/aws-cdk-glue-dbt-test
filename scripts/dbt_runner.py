25005acd40ae94e295aa5eec6731ce3e8b324d6c
Paul Caceres <paul.caceresthamina@externos-cl.cencosud.com>
2025-09-24 17:07:10 -0500

    update dbt runner

--- scripts/dbt_runner.py
+++ scripts/dbt_runner.py
@@ -1,15 +1,46 @@
 import os
 import subprocess
+import logging
+import sys
+import boto3
 
-def main():
-    project_dir = "/tmp/project"
-    profiles_dir = "/tmp/profiles"
+logger = logging.getLogger()
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
 
-    # Download dbt project from S3
-    os.system(f"aws s3 cp s3://vigia-data-lake-dl-artifacts-dbt-dev-a14b31t/dbt_project/ {project_dir} --recursive")
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
 
-    # Run dbt
-    subprocess.run(
-        ["dbt", "run", "--profiles-dir", profiles_dir, "--project-dir", project_dir],
-        check=True
-    )
+logger.info("Logger initialized")
+
+s3 = boto3.client("s3")
+
+bucket = "vigia-data-lake-dl-artifacts-bucket-dev-a14b31"
+prefix = "poc_transformations_dbt/"
+local_dir = "/tmp/project"
+
+os.makedirs(local_dir, exist_ok=True)
+
+def download_prefix(bucket, prefix, local_dir):
+    paginator = s3.get_paginator("list_objects_v2")
+    for page in paginator.paginate(Bucket=bucket, Prefix=prefix):
+        for obj in page.get("Contents", []):
+            key = obj["Key"]
+            if key.endswith("/"):  # skip folders
+                continue
+            local_path = os.path.join(local_dir, key.replace(prefix, "", 1))
+            os.makedirs(os.path.dirname(local_path), exist_ok=True)
+            s3.download_file(bucket, key, local_path)
+
+download_prefix(bucket, prefix, local_dir)
+logger.info(">>> Download complete.")
+
+logger.info(f"Dir: {os.listdir('/tmp/project')}")
+# Run dbt
+logger.info("running dbt")
+result_with_output= subprocess.run(
+    ["dbt", "run", "--profiles-dir", f"{local_dir}/profiles", "--project-dir", local_dir, "--debug"],
+    check=True
+)
+logger.info(result_with_output.stdout)
